1179c3fa39fd80e6a61132b7a646b770fb8594af
Ivan Jurisic <ivan.1jurisic@gmail.com>
2020-12-09 17:50:13 +0100

    updatedFinalProject
    changed some data types and made n to m links

--- finalProject/finalProject.sql
+++ finalProject/finalProject.sql
@@ -3,34 +3,45 @@ create database finalProject;
 
 use finalProject;
 
-create table Klijent (
+create table klijent (
 	sifra int not null primary key auto_increment,
 	ime varchar(50) not null,
 	prezime varchar(50) not null,
 	oib char(11),
 	postanskiBroj varchar(10) not null,
-	email varchar(50) not null,
-	Bankar int
+	email varchar(50) not null
 );
 
-create table Bankar (
+create table bankar (
 	sifra int not null primary key auto_increment,
 	ime varchar(50) not null,
 	prezime varchar(50) not null,
 	email varchar(50) not null
 );
 
-create table KreditnaKartica (
+create table kreditnaKartica (
 	sifra int not null primary key auto_increment,
 	vrijediDo date not null,
 	limitKartice decimal(21,2) not null,
 	brojKartice char(16) not null,
-	Klijent int
+	klijent int
 );
 
-create table Racun (
+create table racun (
 	sifra int not null primary key auto_increment,
 	brojRacuna char(14) not null,
 	stanjeRacuna decimal(21,2),
-	Klijent int
+	klijent int
 );
+
+create table bankar_klijent(
+	sifra int not null primary key auto_increment,
+	klijent int,
+	bankar int
+);
+
+
+alter table racun add foreign key (klijent) references klijent(sifra);
+alter table kreditnaKartica add foreign key (klijent) references klijent(sifra);
+alter table bankar_klijent add foreign key (klijent) references klijent(sifra);
+alter table bankar_klijent add foreign key (bankar) references bankar(sifra);
