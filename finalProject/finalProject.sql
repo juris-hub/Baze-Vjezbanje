55c8e43f41958b45c965906476bd863e6ef6a41b
juris-hub <65715599+juris-hub@users.noreply.github.com>
2020-12-09 13:52:14 +0100

    Prva verzija zavrsnog projekta - SQL

---
+++ finalProject/finalProject.sql
@@ -0,0 +1,36 @@
+drop database if exists finalProject;
+create database finalProject; 
+
+use finalProject;
+
+create table Klijent (
+	sifra int not null primary key auto_increment,
+	ime varchar(50) not null,
+	prezime varchar(50) not null,
+	oib char(11),
+	postanskiBroj varchar(10) not null,
+	email varchar(50) not null,
+	Bankar int
+);
+
+create table Bankar (
+	sifra int not null primary key auto_increment,
+	ime varchar(50) not null,
+	prezime varchar(50) not null,
+	email varchar(50) not null
+);
+
+create table KreditnaKartica (
+	sifra int not null primary key auto_increment,
+	vrijediDo date not null,
+	limitKartice int not null,
+	brojKartice char(16) not null,
+	Klijent int
+);
+
+create table Racun (
+	sifra int not null primary key auto_increment,
+	brojRacuna char(14) not null,
+	stanjeRacuna int,
+	Klijent int
+);
